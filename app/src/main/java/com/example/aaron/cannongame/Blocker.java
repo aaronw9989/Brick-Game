31dd3e5c02947cfcbdcf11f25004462882c213fd
Aaron R. Williams <Aaron@Aarons-MacBook-Pro.local>
2016-11-03 12:08:57 -0400

    Initial commit

---
+++ app/src/main/java/com/example/aaron/cannongame/Blocker.java
@@ -0,0 +1,22 @@
+package com.example.aaron.cannongame;
+
+/**
+ * Created by Aaron on 10/31/16.
+ */
+
+public class Blocker extends GameElement {
+    private int missPenalty; // the miss penalty for this Blocker
+
+    // constructor
+    public Blocker(CannonView view, int color, int missPenalty, int x,
+                   int y, int width, int length, float velocityY) {
+        super(view, color, CannonView.BLOCKER_SOUND_ID, x, y, width, length,
+                velocityY);
+        this.missPenalty = missPenalty;
+    }
+
+    // returns the miss penalty for this Blocker
+    public int getMissPenalty() {
+        return missPenalty;
+    }
+}
